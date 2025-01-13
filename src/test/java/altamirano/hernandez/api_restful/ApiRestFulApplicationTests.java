368d7c65c120a08989464bea62b8e25e183e5aa2
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-01-13 17:14:06 -0600

    API REST

---
+++ src/test/java/altamirano/hernandez/api_restful/ApiRestFulApplicationTests.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.api_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
